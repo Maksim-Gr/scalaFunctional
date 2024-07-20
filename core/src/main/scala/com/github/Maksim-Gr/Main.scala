c2987b45974daba1362efb313cada58c3a992732
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2024-07-20 14:27:22 +0300

    chore: create testing project with template

---
+++ core/src/main/scala/com/github/Maksim-Gr/Main.scala
@@ -0,0 +1,7 @@
+package com.github/Maksim-Gr
+
+
+object Main {
+  def main(args: Array[String]): Unit = println("Hello World")
+}
+
---
+++ tests/src/test/scala/com/github/Maksim-Gr/tests/CatsfunctionalTestSuite.scala
@@ -0,0 +1,11 @@
+package com.github/Maksim-Gr
+package tests
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.funsuite.AnyFunSuiteLike
+
+class CatsfunctionalTestSuite extends AnyFunSuiteLike with Matchers {
+  test("test") {
+    1 shouldBe 1
+  }
+}
