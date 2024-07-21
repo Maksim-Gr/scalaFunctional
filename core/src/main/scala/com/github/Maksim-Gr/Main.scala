2b866d8a887beb214018637d62356c89da5f90f4
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-07-21 20:28:32 +0300

    chore: concept of structural corecursion

--- core/src/main/scala/com/github/Maksim-Gr/Main.scala
+++ core/src/main/scala/com/github/Maksim-Gr/Main.scala
@@ -3,5 +3,16 @@ package com.github/Maksim-Gr
 
 object Main {
   def main(args: Array[String]): Unit = println("Hello World")
+
+
+  enum myList[A] {
+    case Empty()
+    case Pair(head: A, tail: MyList[A])
+
+
+    def map[B](f: A => B): MyList[B] =
+      if ??? then Empty()
+      else Pair(???, ???)
+  }
 }
 
